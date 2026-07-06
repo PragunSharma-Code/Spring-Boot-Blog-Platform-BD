8bc684a542a3c1e4511cba9bb58df5d9c28f24bd
PragunSharma-Code <pragunsharma.business@gmail.com>
2026-07-06 14:37:35 +0530

    Added Relationships between Entities

--- src/main/java/com/devtiro/blog/domain/entities/Tag.java
+++ src/main/java/com/devtiro/blog/domain/entities/Tag.java
@@ -3,8 +3,7 @@ package com.devtiro.blog.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,6 +20,9 @@ public class Tag {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
